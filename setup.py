5ae966c817ff144a1ceffbd507e41b0f4f106ee9
Eric Langlois <contact@eric-langlois.ca>
2015-06-28 12:06:08 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="pygdl",
+      version="0.1",
+      description="Game Description Language (GDL) library",
+      url="",
+      author="Eric Langlois",
+      author_email="eric@langlois.xyz",
+      license="MIT",
+      packages=["pygdl"],
+      install_requires=["pyswip"],
+)
