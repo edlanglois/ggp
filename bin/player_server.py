bbbbc742e28c213912456440133477813772f7cd
Eric Langlois <eric@langlois.xyz>
2015-07-26 17:10:48 -0400

    Player server has command line arguments

--- bin/player_server.py
+++ bin/player_server.py
@@ -1,11 +1,36 @@
 #!/usr/bin/python3
 
+import argparse
 import logging
 
 from pygdl.gamestate import GameState
 from pygdl.players import LegalGamePlayer
 from pygdl.playerserver import run_player_server
 
-logging.basicConfig(level=logging.DEBUG)
+player_classes = [LegalGamePlayer]
+player_class_dict = {class_.__name__: class_
+                     for class_ in player_classes}
 
-run_player_server(LegalGamePlayer, GameState)
+class LogLevel(object):
+    levels = ['debug', 'info', 'warning', 'error', 'critical']
+    def __init__(self, level_string):
+        if level_string in self.levels:
+            self.level = getattr(logging, level_string.upper())
+        else:
+            self.level = int(level_string)
+
+parser = argparse.ArgumentParser(description="General Game Player Server")
+parser.add_argument('player', type=str,
+                    choices=player_class_dict.keys(),
+                    help="Game player class.")
+parser.add_argument('-P', '--port', type=int, default=9147,
+                    help="Port on which the server listens.")
+parser.add_argument('--log-level', type=LogLevel, default='info',
+                    help=("Set logging level. "
+                          "Either an integer value or one of: "
+                          "{} (default '{}')".format(LogLevel.levels,
+                                                     'info')))
+args = parser.parse_args()
+
+logging.basicConfig(level=args.log_level.level)
+run_player_server(player_class_dict[args.player], GameState)
