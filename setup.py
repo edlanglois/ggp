b3d515f2b36302913bdb4a4a36e2fa76b3d68bb3
Eric Langlois <eric@langlois.xyz>
2015-10-18 16:11:15 -0400

    Added requirements.txt

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
-setup(name="pygdl",
-      version="0.1",
-      description="Game Description Language (GDL) library",
-      url="",
-      author="Eric Langlois",
-      author_email="eric@langlois.xyz",
-      license="MIT",
-      packages=["pygdl"],
-      install_requires=[
-        "pyswip"
-      ],
+setup(
+    name="pygdl",
+    version="0.1",
+    description="Game Description Language (GDL) library",
+    url="",
+    author="Eric Langlois",
+    author_email="eric@langlois.xyz",
+    license="MIT",
+    packages=["pygdl"],
+    install_requires=[
+        "pyswip>=0.2.5"
+    ],
 )
