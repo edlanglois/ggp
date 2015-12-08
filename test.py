e2d77c896d2205e5af3fe5ee2bdcf1cd684d0f94
Eric Langlois <eric@langlois.xyz>
2015-12-08 04:35:52 -0500

    Removed test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from pygdl.pyswipl.prolog import *
-
-f = Functor(Atom('foo'), 1)
-print(str(f), repr(f))
-t = Term.from_functor(f)
-print(str(t), repr(t))
-t0 = t[0]
-print(str(t0))
-print(repr(t0))
